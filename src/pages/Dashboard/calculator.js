da9733be1037ce5cfe57e85bdefa741814a8e11d
PritiShaw <pritishaw0103@gmail.com>
2020-06-27 22:03:00 +0530

    Handle Supplier listing

--- src/pages/Dashboard/calculator.js
+++ src/pages/Dashboard/calculator.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./calculator.css";
 import "../../App.css";
-import { Form } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Col from "react-bootstrap/Col"
-import { Link } from "react-router-dom"
 import QuestionHandler from "./Questions"
 
 import "./calculator.css";
@@ -15,7 +11,7 @@ const Calculator = (props) => {
             <div className="col-12 py-5">
                 <h2 id="ques">Recovery Score Calculator</h2>
                 <h4 id="ques">Fill out the following information to know your Recovery Score</h4>
-                <hr />
+                <br />
                 <QuestionHandler userid={props.userid} />
             </div>
         </div>
