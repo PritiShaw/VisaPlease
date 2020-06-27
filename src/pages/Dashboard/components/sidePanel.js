d14e3b749a3bf67f2f50ec91b7cf462a5d37bf03
Navalika Sharda <shardanavalika@gmail.com>
2020-06-27 17:08:58 +0530

    dashboard

--- src/pages/Dashboard/components/sidePanel.js
+++ src/pages/Dashboard/components/sidePanel.js
@@ -7,7 +7,11 @@ const sidePanel = () => {
   return (
     <div>
     <br/><br/>
+    
       <ul className="sidepanel">
+      <li>
+          <Link to="/dashboard" className="link">Dashboard</Link>
+        </li>
         <li>
           <Link to="/dashboard/calculator" className="link">Calculator</Link>
         </li>
@@ -15,9 +19,7 @@ const sidePanel = () => {
           <Link to="/dashboard/locator" className="link">Merchant</Link>
           {/* checkakr lena parht */}
         </li>
-        <li>
-          <Link to="/dashboard/track" className="link">Track</Link>
-        </li>
+        
       </ul>
     </div>
   );
