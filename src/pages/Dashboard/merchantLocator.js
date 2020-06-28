97a0c055427b1ec0cd0fcee86ae5fa7a433d9ce9
PritiShaw <pritishaw0103@gmail.com>
2020-06-28 22:06:29 +0530

    Add MultiSelect Category List

--- src/pages/Dashboard/merchantLocator.js
+++ src/pages/Dashboard/merchantLocator.js
@@ -10,10 +10,16 @@ const MerchantLocator = () => {
   const [radiusUnit, setRadiusUnit] = useState("M");
   const [merchant_Name, setMerchant_Name] = useState("");
   const [merchantCountryCode, setMerchantCountryCode] = useState("");
-  const [merchantCategory, setMerchantCategory] = useState("1750");
+  const [merchantCategory, setMerchantCategory] = useState(["1750"]);
   const [startIndex, setStartIndex] = useState(0);
   const [criteriaSelector, setCriteriaSelector] = useState(0);
 
+  const handleCategoryCode = (event) => {
+    let newCategoryList = [];
+    [...event.target.options].filter(o => o.selected).map(o => newCategoryList.push(o.value))
+
+    setMerchantCategory(newCategoryList)
+  }
   const searchButtonClick = () => {
     var result = [];
     setSearchResults([]);
@@ -29,7 +35,7 @@ const MerchantLocator = () => {
         distanceUnit: radiusUnit,
         merchantName: merchant_Name,
         countryCode: merchantCountryCode,
-        categoryCode: criteriaSelector == 1 ? [merchantCategory] : [],
+        categoryCode: criteriaSelector == 1 ? merchantCategory : [],
         criteriaSelector: criteriaSelector,
         startIndex: startIndex,
       }),
@@ -110,21 +116,21 @@ const MerchantLocator = () => {
               {criteriaSelector == 1 ? (
                 <h3 className="my-4 text-center">Merchant Details</h3>
               ) : (
-                <>
-                  <input
-                    className="form-control"
-                    placeholder="Merchant Name"
-                    value={merchant_Name}
-                    onChange={(e) => setMerchant_Name(e.target.value)}
-                  />
-                  <input
-                    className="form-control"
-                    placeholder="Merchant Country code"
-                    value={merchantCountryCode}
-                    onChange={(e) => setMerchantCountryCode(e.target.value)}
-                  />
-                </>
-              )}
+                  <>
+                    <input
+                      className="form-control"
+                      placeholder="Merchant Name"
+                      value={merchant_Name}
+                      onChange={(e) => setMerchant_Name(e.target.value)}
+                    />
+                    <input
+                      className="form-control"
+                      placeholder="Merchant Country code"
+                      value={merchantCountryCode}
+                      onChange={(e) => setMerchantCountryCode(e.target.value)}
+                    />
+                  </>
+                )}
             </div>
           </div>
           <div className="col-sm-2 text-center v-100">
@@ -135,22 +141,22 @@ const MerchantLocator = () => {
               {criteriaSelector == 0 ? (
                 <h3 className="my-4 text-center">Merchant Category</h3>
               ) : (
-                <>
-                  <label>Merchant Category</label>
-                  <select
-                    className="form-control"
-                    value={merchantCategory}
-                    onChange={(e) => setMerchantCategory(e.target.value)}
-                  >
-                    <option value="1750">Carpentery</option>
-                    <option value="5137">Clothing</option>
-                    <option value="5039">Construction</option>
-                    <option value="5942">Stationary</option>
-                    <option value="5045">Computer</option>
-                    <option value="5251">Hardware</option>
-                  </select>
-                </>
-              )}
+                  <>
+                    <label>Merchant Category</label>
+                    <select multiple
+                      className="form-control selectpicker"
+                      value={merchantCategory}
+                      onChange={(e) => handleCategoryCode(e)}
+                    >
+                      <option value="1750">Carpentery</option>
+                      <option value="5137">Clothing</option>
+                      <option value="5039">Construction</option>
+                      <option value="5942">Stationary</option>
+                      <option value="5045">Computer</option>
+                      <option value="5251">Hardware</option>
+                    </select>
+                  </>
+                )}
             </div>
           </div>
           <div className="col-sm-12"></div>
@@ -209,10 +215,10 @@ const MerchantLocator = () => {
               );
             })
           ) : (
-            <tr>
-              <td colSpan="5">No results found</td>
-            </tr>
-          )}
+              <tr>
+                <td colSpan="5">No results found</td>
+              </tr>
+            )}
         </table>
       </div>
     </div>
