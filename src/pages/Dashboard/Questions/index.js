cccb869298f1f5bebead6ffeb4247059e09de3ae
Yash Goyal <yash.goyal.mat16@itbhu.ac.in>
2020-06-29 22:49:56 +0530

    Update MerchantMeasurement (#25)
    * Update MerchantMeasurement
    
    * Update index.js
    
    Co-authored-by: Priti Shaw <pritishaw0103@gmail.com>

--- src/pages/Dashboard/Questions/index.js
+++ src/pages/Dashboard/Questions/index.js
@@ -30,7 +30,7 @@ const Questions = () => {
 
 
   const submitAll = async () => {
-    var answers_with_scores = calculateRecoveryScore(userid, answers);
+    var answers_with_scores = await calculateRecoveryScore(userid, answers);
     if (await storeRecoveryQuestionnaire(userid, answers_with_scores)) {
       history.push("/dashboard/ScoreDisplay");
       window.location.reload();
