82cadd4719949a0455cbb53929f8414a1f8eab55
PritiShaw <pritishaw0103@gmail.com>
2020-06-30 17:38:46 +0530

    Modify signup page

--- src/pages/Auth/SignUp.js
+++ src/pages/Auth/SignUp.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { auth } from "../../firebaseConfig";
 import { generateUserDocument } from "../../utils/firestore";
 import "../Dashboard/calculator.css"
+
 const SignIn = () => {
   const history = useHistory()
   const [firstName, setFirstName] = useState("");
@@ -14,6 +15,8 @@ const SignIn = () => {
   const [countryCode, setCountryCode] = useState("");
   const [visaStoreId, setVisaStoreId] = useState("");
   const [categoryCode, setCategoryCode] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [page, setPage] = useState(0);
 
   const [error, setError] = useState(null);
   const registerAccount = async () => {
@@ -22,7 +25,7 @@ const SignIn = () => {
         email,
         password
       );
-      await generateUserDocument(user, { firstName, lastName, companyName, countryCode, visaStoreId, categoryCode });
+      await generateUserDocument(user, { firstName, lastName, companyName, countryCode, visaStoreId, categoryCode, postalCode });
       history.push("/dashboard")
       window.location.reload()
     } catch (err) {
@@ -31,110 +34,130 @@ const SignIn = () => {
   };
 
   return (
-    <div className="sign-up mt-5">
-      
-      <div id="log" className="pt-5">
-        <h3>Sign Up</h3>
+    <>
+      <div className="sign-up">
+        <div>
+          <h3>Sign Up</h3>
+          {(page == 0) ? (
+            <>
+              <div className="form-group">
+                <label>First name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
 
-        <div className="form-group">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
+              <div className="form-group">
+                <label>Last name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
 
-        <div className="form-group">
-          <label>Last name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Last name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
+              <div className="form-group">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
 
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button 
+                type="submit"
+                className="btn btn-primary btn-block"
+                onClick={() => setPage(1)}
+              >
+                Next
+              </button>
+            </>
+          ) : (
+              <>
+                <div className="form-group">
+                  <label>Company name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Company name"
+                    value={companyName}
+                    onChange={(e) => setCompanyName(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Country Code</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Country code"
+                    value={countryCode}
+                    onChange={(e) => setCountryCode(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Visa Store Id</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Visa Store Id"
+                    value={visaStoreId}
+                    onChange={(e) => setVisaStoreId(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Merchant category code</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Category code"
+                    value={categoryCode}
+                    onChange={(e) => setCategoryCode(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Merchant Postal Code</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Category code"
+                    value={postalCode}
+                    onChange={(e) => setPostalCode(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block"
+                  onClick={() => registerAccount()}
+                >
+                  Sign Up
+                </button>
+              </>
+            )}
+          <p className="forgot-password text-right">
+            Already registered <Link to="/auth/login">sign in?</Link>
+          </p>
         </div>
-
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Company name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Company name"
-            value={companyName}
-            onChange={(e) => setCompanyName(e.target.value)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Country Code</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Country code"
-            value={countryCode}
-            onChange={(e) => setCountryCode(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label>Visa Store Id</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Visa Store Id"
-            value={visaStoreId}
-            onChange={(e) => setVisaStoreId(e.target.value)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Merchant category code</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Category code"
-            value={categoryCode}
-            onChange={(e) => setCategoryCode(e.target.value)}
-          />
-        </div>
-
       </div>
-      <button
-        type="submit"
-        className="btn btn-primary btn-block"
-        onClick={() => registerAccount()}
-      >
-        Sign Up
-      </button>
-      <p className="forgot-password text-right">
-        Already registered <Link to="/auth/login">sign in?</Link>
-      </p>
-    </div>
+    </>
   );
 };
 export default SignIn;
